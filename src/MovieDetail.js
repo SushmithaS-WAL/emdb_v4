2ac49a627ac5785a46f5a8e6c4beb8a3961a799b
EmmanuelCBaby <emmanuel.b@westagilelabs.com>
2018-03-09 06:44:45 +0530

    mini-project

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -8,24 +8,36 @@ class Moviedetail extends Component {
 
 
     render(){
-        return(
-            <div className="Slideshow">
-            {this.props.moviedetails.map((element,index)=>{
-                return(
-                    <div key={index}>
-                    <p className="searchresult-div-title">{element.title}</p>
-                    <img className="searchresult-image" src={`https://image.tmdb.org/t/p/w92/${element.poster_path}`} alt="poster" />
-                    <p className="searchresult">Rating: </p><label className="searchcontent">{element.vote_average}</label>
-                    <p className="searchresult">Language: </p><label className="searchcontent">{element.original_language}</label>
-                    <p className="searchresult">Overview: </p><label className="searchcontent">{element.overview}</label>
-                    <p className="searchresult">Release Date: </p><label className="searchcontent">{element.release_date.split('T00:00:00.000Z')}</label>
-                    <br />
-                    <button id={element.id} className="searchresult-read-more" onClick={this.moreDetails}>Read More</button>
-                    </div>
-                )
-            })}
+        var Moviedetail=(
+            <div className="Moviefulldetail">
+            <header className="App-header">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+            </header>
+            <div>
+                {this.props.moviedetails.map((element,index)=>{
+                    return(
+                        <div key={index}>
+                        <p className="Moviefulldetail-title">{element.title}</p>
+                        <img className="Moviefulldetail-image" src={`https://image.tmdb.org/t/p/w154/${element.poster_path}`} alt="poster" />
+                        <div className="Moviefulldetail-rating">
+                            <p className="searchresult">Rating:{element.vote_average}</p>
+                        </div>
+                        <div className="Moviefulldetail-language">
+                        <p className="searchresult">Language:{element.original_language}</p>
+                        </div>
+                        <p className="searchresult">Overview: </p><label className="searchcontent">{element.overview}</label>
+                        <p className="searchresult">Release Date: </p><label className="searchcontent">{element.release_date.split('T00:00:00.000Z')}</label>
+                        <br />
+                        <button id={element.id} className="searchresult-read-more" onClick={this.moreDetails}>Read More</button>
+                        </div>
+                    )
+                })}
+            </div>
             </div>
         )
+
+        return(Moviedetail);
     }
 }
 
